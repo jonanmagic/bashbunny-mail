e771472bfa94c54491e6f8ecb10362d48d57f526
jonanmagic <56024075+jonanmagic@users.noreply.github.com>
2019-11-13 10:12:44 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,13 +1,7 @@
 $Outlook = New-Object -ComObject Outlook.Application
 $Mail = $Outlook.CreateItem(0)
-$Mail.To = "virgilio.garcia@arcelormittal.com"
-$Mail.Subject = "Important: Fringe presentation"
-$Mail.Body = "Good morning Virgilio. 
-
-How was the presentation? I think we have to increase the salary of the guys. 
-Let's talk later. 
-
-Regards, Dani 
-"
+$Mail.To = "test@#test.com"
+$Mail.Subject = "Your Subject"
+$Mail.Body = "Your body"
 $Mail.Send()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Outlook) | Out-Null
